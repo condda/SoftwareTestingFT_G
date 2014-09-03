d0977431e67a0be72040f3968e2978120ef41892
Wouter Nederhof <wouter@nedtalk.com>
2014-09-03 16:40:11 +0200

    Finished all assignments, except 2.13.

--- wouter/week-1/Sol2.hs
+++ wouter/week-1/Sol2.hs
@@ -2,15 +2,19 @@ module Sol2 where
 
 --import TAMO
 
--- 2.13
--- -lequiv
+-- TODO 2.13
 
 -- 2.51
-unique :: (a -> Bool) -> [a] -> Bool
---unique _ [] | false
-unique p (x:xs) | p . x && (not (unique p xs))
+unique :: Eq a => (a -> Bool) -> [a] -> Bool
+unique _ [] = False
+unique p (x:xs) = (p x) && (not (unique p xs))
 
 -- 2.52
-
+parity :: [Bool] -> Bool
+parity [] = True
+parity (x:xs) | x = not (parity xs)
+	      | otherwise = parity xs
 
 -- 2.53
+evenNR :: Eq a => (a -> Bool) -> [a] -> Bool
+evenNR p xs = parity (map p xs)
