4b3435ebe101cbaa2dd837343fbf0079bf8782a6
Wouter Nederhof <wouter@nedtalk.com>
2014-09-16 23:49:50 +0200

    bonus2 edited.

--- week_2_group_FT_G/bonus1.hs
+++ week_2_group_FT_G/bonus1.hs
@@ -1,4 +1,4 @@
-module Test
+module Bonus1
 
 where
 
--- week_2_group_FT_G/bonus2.hs
+++ week_2_group_FT_G/bonus2.hs
@@ -1,5 +1,9 @@
-number_of_derangements :: Int -> Int
-number_of_derangements 1 = 1
-number_of_derangements x
-  | even x = (number_of_derangements (x-1) ) * (x-1) - 1
-  | otherwise  = (number_of_derangements (x-1) ) * (x-1) + 1
+module Bonus2
+
+where
+
+-- We found the following function:
+numberOfDerangements :: Int -> Int
+numberOfDerangements 1 = 1
+numberOfDerangements x | even x = (numberOfDerangements (x-1) ) * (x-1) - 1
+                       | otherwise  = (numberOfDerangements (x-1) ) * (x-1) + 1
